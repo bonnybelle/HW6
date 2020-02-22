db87fe9e080e64a40955a309596401b69177e497
bonnybelle <55929630+bonnybelle@users.noreply.github.com>
2020-02-22 20:14:15 +0200

    Delete task5.py

--- task5.py
+++
@@ -1,6 +0,0 @@
-# 5. Дан список большой вложенности, нужно его развернуть в 1d
-
-lst = [[], [1, 2], [2, 3, [1, 2]], [1, 2, 3]]
-temporary_str = str(lst).replace('[', '').replace(']', '')
-final = [int(i) for i in temporary_str if i.isdigit()]
-print(final)
